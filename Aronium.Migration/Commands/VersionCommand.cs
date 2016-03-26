4f4479ee92ad6a582877dfb99a9ae5064df2bdc0
Aronium <bosko.kukanjac@ghcpro.com>
2016-03-26 12:42:33 +0100

    Get version command

---
+++ Aronium.Migration/Commands/VersionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aronium.Migration.Commands
+{
+    [Command("version", "Gets current version for specified module.")]
+    public class VersionCommand : DataCommandBase
+    {
+        public override void Run(InputArguments args)
+        {
+            var module = args["module"];
+
+            var version = GetCurrentVersion(module);
+
+            Console.WriteLine(string.Format("Current version: {0}", version));
+            Console.WriteLine(string.Format("Selected module: {0}", module));
+        }
+    }
+}
